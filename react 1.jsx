23a4accf3e0c4e6b92aef5056c1c9e3c98e4eed9
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-05-21 09:30:15 +0530

    Add initial React component with navigation and greeting

---
+++ react 1.jsx
@@ -0,0 +1,18 @@
+
+let name = "sudhanshu";
+function App() {
+  return (
+    <>
+      <nav>
+        <li>Home</li>
+        <li>about</li>
+        <li>profile</li>
+      </nav>
+
+      <div className="container">
+        <h1>Hello {name}</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus quis ad asperiores reiciendis ex laudantium laboriosam repellendus ipsa atque magnam facilis, accusantium voluptatibus pariatur quae! Omnis voluptas eaque ipsa sed?</p></div>
+    </>
+  );
+}
+export default App;
